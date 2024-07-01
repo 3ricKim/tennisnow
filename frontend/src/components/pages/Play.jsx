bf31d395d61941b8265af4410a072226d40d3b9b
3ricKim <erickim2281@gmail.com>
2024-07-02 02:21:11 +0900

    Added Map and Calandar
    Basic calandar and maps using google api

--- frontend/src/components/pages/Play.jsx
+++ frontend/src/components/pages/Play.jsx
@@ -1,10 +1,45 @@
-export const Play = () => {
-  return (
-    <div>
-      Play
-    </div>
-  )
-}
-// features for meeting casual player, finding/hosting tournament, finding court
+import { useState, useEffect } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+import { Map } from "@vis.gl/react-google-maps";
 
+import "./Play.css";
+
+export const Play = () => {
+  const [value, setValue] = useState(() => {
+    const storedDate = localStorage.getItem("selectedDate");
+    return storedDate ? new Date(storedDate) : new Date();
+  });
+
+  useEffect(() => {
+    localStorage.setItem("selectedDate", value.toISOString());
+    console.log("Current date value:", value);
+  }, [value]);
+
+  return (
+    <div className="play-container">
+      <header>
+        <h3>Choose a Date and Location</h3>
+      </header>
+      <div className="calandar-container">
+        <Calendar onChange={setValue} value={value} />
+      </div>
+      <div className="map-container">
+        <Map
+          defaultZoom={13}
+          defaultCenter={{ lat: -33.860664, lng: 151.208138 }}
+          onCameraChanged={(ev) => {
+            console.log(
+              "camera changed:",
+              ev.detail.center,
+              "zoom:",
+              ev.detail.zoom
+            );
+          }}
+        ></Map>
+      </div>
+    </div>
+  );
+};
+// features for meeting casual player, finding/hosting tournament, finding court
 export default Play;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,19 +4,29 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import { ClerkProvider } from "@clerk/clerk-react";
+import {APIProvider} from '@vis.gl/react-google-maps';
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+const GOOGLEMAP_KEY = import.meta.env.VITE_GOOGLEMAP_APIKEY;
 
 if (!PUBLISHABLE_KEY) {
-  throw new Error("Missing Publishable Key");
+  throw new Error("Missing Clerk Publishable Key");
 }
+
+if (!GOOGLEMAP_KEY) {
+  throw new Error("Missing Google Map API Key")
+}
+
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <APIProvider apiKey={GOOGLEMAP_KEY} onLoad={() => console.log('Maps API has loaded.')}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </APIProvider>
     </ClerkProvider>
   </React.StrictMode>
 );
